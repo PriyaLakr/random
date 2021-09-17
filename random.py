6e0776d3c8e9f44022742c7f8fb4a63767b14d84
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-09-17 15:02:16 +0530

    Update random.py

--- random.py
+++ random.py
@@ -1,4 +1,16 @@
+# pythonic ways!
+
 info = [("Pri", 35), ("Ada", 50), ("Meg", 27), ("Sar", 37)]
 
 # sorting list from oldest "Ada" to youngest "Meg"
 list(reversed(sorted(info, key=lambda x: x[1])))
+
+
+lst = [1,2,3,4]
+
+# map, lambda
+list(map( lambda x: x*x, lst ))
+> [1, 4, 9, 16]
+
+list(map( lambda x: x/x, lst ))
+> [1.0, 1.0, 1.0, 1.0]
